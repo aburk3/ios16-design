bc986b4f3e3b9e4c55fe93bff0c908ec9ede5071
Austin Burke <austinlayneburke@gmail.com>
2023-03-29 06:33:49 -0600

    Initial Commit

---
+++ DesignCodeiOS16/DesignCodeiOS16App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS16App.swift
+//  DesignCodeiOS16
+//
+//  Created by Austin Burke on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
