433fe0307185e54bcc0b8d108fd176c5c93970a3
Austin Burke <austinlayneburke@gmail.com>
2023-04-03 06:38:17 -0600

    Initial commit

--- DesignCodeiOS16/ContentView.swift
+++ DesignCodeiOS16/ContentView.swift
@@ -9,18 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
+        VStack(alignment: .center, spacing: 20.0) {
+            Image(systemName: "timelapse", variableValue: 0.2)
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                .font(.system(size: 50))
+                .fontWeight(.thin)
+            Text("Switching Apps".uppercased())
+                .font(.largeTitle.width(.condensed))
+                .fontWeight(.bold)
+            Text("Tap and hold any part of the screen for 1 second to show the menu for switching between apps.")
+                .multilineTextAlignment(.center)
+                .foregroundColor(.secondary)
+                .fontWeight(.medium)
+            Button {
+                // Action
+            } label: {
+                Text("Got it")
+                    .padding(.all)
+                    .frame(maxWidth: .infinity)
+                    .background(.white.opacity(0.2).gradient)
+                    .cornerRadius(10)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke()
+                            .foregroundColor(.secondary)
+                    )
+                    
+            }
+            .accentColor(.primary)
         }
-        .padding()
+        .padding(30)
+        .background(.ultraThinMaterial)
+        .cornerRadius(10)
+        .overlay(RoundedRectangle(cornerRadius: 10).stroke())
+        .padding(10)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .background(Image("Wallpaper 2"))
+
     }
 }
