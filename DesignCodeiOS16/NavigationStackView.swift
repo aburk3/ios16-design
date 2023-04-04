a702e5b3ac7cf206119cab94479a65ca79000349
Austin Burke <austinlayneburke@gmail.com>
2023-04-04 06:34:55 -0600

    Add navigation stack view data

--- DesignCodeiOS16/NavigationStackView.swift
+++ DesignCodeiOS16/NavigationStackView.swift
@@ -10,10 +10,28 @@ import SwiftUI
 struct NavigationStackView: View {
     var body: some View {
         NavigationStack {
-            List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
-                NavigationLink(destination: Text("Context")) {
-                    Label("Item", systemImage: "house")
+            List(navigationItems) { item in
+                NavigationLink(value: item) {
+                    Label(item.title, systemImage: item.icon)
                         .foregroundColor(.primary)
+                }
+            }
+            .navigationDestination(for: NavigationItem.self) { item in
+                switch item.menu {
+                case .compass:
+                    ContentView()
+                case .card:
+                    Text(item.title)
+                case .charts:
+                    Text(item.title)
+                case .radial:
+                    Text(item.title)
+                case .halfsheet:
+                    Text(item.title)
+                case .gooey:
+                    Text(item.title)
+                case .actionbutton:
+                    Text(item.title)
                 }
             }
             .navigationTitle("SwiftUI Apps")
