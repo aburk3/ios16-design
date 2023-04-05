4c9ea40b44b72595f304ce492be03f25a1947247
Austin Burke <austinlayneburke@gmail.com>
2023-04-05 07:00:55 -0600

    Finish up ratings component

--- DesignCodeiOS16/GridView.swift
+++ DesignCodeiOS16/GridView.swift
@@ -15,22 +15,46 @@ struct GridView: View {
                 Text("Votes")
                     .gridColumnAlignment(.trailing)
                     .gridCellColumns(2)
-                Text("Percentage")
+                Text("Rating")
+                    .gridColumnAlignment(.trailing)
 
             }
+            .font(.footnote)
+            .foregroundColor(.secondary)
+
             Divider()
                 .gridCellUnsizedAxes(.horizontal)
+
             GridRow {
                 Text("4")
+                    .frame(width: 30, alignment: .trailing)
                 ProgressView(value: 0.1)
                     .frame(maxWidth: 250)
-                Image(systemName: "star")
+                RatingView(rating: 1)
             }
             GridRow {
                 Text("21")
                 ProgressView(value: 0.2)
                     .frame(maxWidth: 250)
-                Image(systemName: "star")
+                RatingView(rating: 2)
+            }
+            GridRow {
+                Text("5")
+                ProgressView(value: 0.1)
+                    .frame(maxWidth: 250)
+                RatingView(rating: 3)
+            }
+            GridRow {
+                Text("52")
+                ProgressView(value: 0.5)
+                    .frame(maxWidth: 250)
+                RatingView(rating: 4)
+            }
+            GridRow {
+                Text("67")
+                ProgressView(value: 0.6)
+                    .frame(maxWidth: 250)
+                RatingView(rating: 5)
             }
         }
         .padding(20)
@@ -40,5 +64,18 @@ struct GridView: View {
 struct GridView_Previews: PreviewProvider {
     static var previews: some View {
         GridView()
+    }
+}
+
+struct RatingView: View {
+    var rating = 3
+    let list = [1, 2, 3, 4, 5]
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(list, id: \.self) { item in
+                Image(systemName: rating >= item ?
+                    "star.fill" : "star")
+            }
+        }
     }
 }
